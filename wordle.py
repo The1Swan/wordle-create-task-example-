e19d693e2d3b0fd649c410f9b3080a4578e0569c
The1Swan N/A <isabella.ann.swan@gmail.com>
2024-09-16 18:25:51 +0000

    wordle code

---
+++ wordle.py
@@ -0,0 +1,34 @@
+word = "tempo"
+
+#colors for printing
+default = '\033[0m'
+green = '\033[92m'
+yellow = '\033[33m'
+
+#generate hint with correct color letters
+def generate_hint(user_guess):
+    color = default
+    hint = ""
+    for i in range(5):
+        if (user_guess[i] == word[i]):
+            color = green
+        elif (user_guess[i] in word):
+            color = yellow
+        else:
+            color = default
+        hint = hint + color + user_guess[i] + default
+    
+    return hint
+
+
+def game_loop():
+    #loop in 6 times
+    guess = ""
+    for i in range(6):
+        guess = input("what is your guess: ")
+        print(generate_hint(guess))
+        if (guess == word):
+            print("congradulations!!")
+            break
+
+game_loop()
